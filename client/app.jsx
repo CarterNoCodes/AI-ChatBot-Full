f575f75e6ca2b56fb272c399cff13aca63152267
Caine <caineheathcote@gmail.com>
2024-08-22 13:07:12 +0100

    Add error boundary and debug logs

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ChatbotApp from './src/Components/ChatBotApp/ChatBotApp';
+import ErrorBoundary from './src/ErrorBoundary';
 
 function App() {
   return (
-    <div className="App">
-      <ChatbotApp />
-    </div>
+    <ErrorBoundary>
+      <div className="App">
+        <ChatbotApp />
+      </div>
+    </ErrorBoundary>
   );
 }
 
